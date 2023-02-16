19ae16209d01749b8562cd99dca28d694499219d
AbdElRahmanLotfy <abdelrahmannnn11@gmail.com>
2023-02-16 10:35:39 +0200

    last-updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from "three";
+import "./App.css";
+import { Suspense, useEffect, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Reflector, Text, useTexture } from "@react-three/drei";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      concurrent="true"
+      gl={{ alpha: false }}
+      pixelratio={[1, 1.5]}
+      camera={{ position: [0, 3, 100], fov: 15 }}
+    >
+      <color attach="background" args={["black"]} />
+      <fog attach="fog" args={["black", 15, 20]} />
+      <Suspense fallback={null}>
+        <group position={[0, -1, 0]}>
+          <VideoText position={[0, 1.3, -2]} />
+          <Ground />
+        </group>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[0, 10, 0]} intensity={0.3} />
+        <directionalLight position={[-50, 0, -40]} intensity={0.7} />
+        <Intro />
+      </Suspense>
+    </Canvas>
   );
+}
+
+function VideoText(props) {
+  const [video] = useState(() =>
+    Object.assign(document.createElement("video"), {
+      src: "/Motion-Graphics-ShowReel-2017.mp4",
+      crossOrigin: "Anonymous",
+      loop: true,
+      muted: true,
+    })
+  );
+  useEffect(() => void video.play(), [video]);
+  return (
+    <Text fontSize={3} letterSpacing={-0.06} {...props}>
+      HOPE
+      <meshBasicMaterial toneMapped={false}>
+        <videoTexture
+          attach="map"
+          args={[video]}
+          encoding={THREE.sRGBEncoding}
+        />
+      </meshBasicMaterial>
+    </Text>
+  );
+}
+
+function Ground() {
+  const [floor, normal] = useTexture(["/DhhEEcxW0AAdPil.jpeg", "/pzvSU.jpg"]);
+  return (
+    <Reflector
+      blur={[400, 100]}
+      resolution={512}
+      args={[10, 10]}
+      mirror={1}
+      mixBlur={6}
+      mixStrength={1.5}
+      rotation={[-Math.PI / 2, 0, Math.PI / 2]}
+    >
+      {(Material, props) => (
+        <Material
+          color="#a0a0a0"
+          metalness={0.4}
+          roughnessMap={floor}
+          normalMap={normal}
+          normalScale={[2, 2]}
+          {...props}
+        />
+      )}
+    </Reflector>
+  );
+}
+
+function Intro() {
+  const [vec] = useState(() => new THREE.Vector3());
+  return useFrame((state) => {
+    state.camera.position.lerp(
+      vec.set(state.mouse.x * 4, 3 + state.mouse.y * 2, 14),
+      0.05
+    );
+    state.camera.lookAt(0, 0, -5);
+  });
 }
 
 export default App;
